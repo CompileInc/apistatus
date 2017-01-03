46bf84c8db206aaa847b96b77b589e3039ae6f4c
Cyriac Thomas <cyriac@compile.com>
2017-01-04 01:21:13 +0530

    copy changes, set status tab from url

--- js/main.js
+++ js/main.js
@@ -12,4 +12,11 @@ $(function(){
     $(".refresh-status").click(function(){
         updateStatusPages();
     });
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-pills a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+    $('.nav-pills a').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+    })
 });
