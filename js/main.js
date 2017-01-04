36c9f451e7fc3272be23cb5393e09b6f7406ee14
Cyriac Thomas <cyriac@compile.com>
2017-01-04 10:11:00 +0530

    adjusts height on iframe

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(function(){
     if (url.match('#')) {
         $('.nav-pills a[href="#' + url.split('#')[1] + '"]').tab('show');
     }
-    $('.nav-pills a').on('shown.bs.tab', function (e) {
-        window.location.hash = e.target.hash;
-    })
+    // $('.nav-pills a').on('shown.bs.tab', function (e) {
+    //     window.location.hash = e.target.hash;
+    // })
 });
